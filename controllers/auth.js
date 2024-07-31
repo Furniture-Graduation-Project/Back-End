590a6e6482a52cc5c5ec138c99405e6ec7b51de9
RiverNguyen <ngiang1309@gmail.com>
2024-07-31 21:53:12 +0700

    update folder and file

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,85 +2,82 @@ import User from "../models/user.js";
 import { StatusCodes } from "http-status-codes";
 import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
-import { signupSchema, signinSchema } from "../schemas/auth.js";
+import { signupSchema, signinSchema } from "../validations/auth.js";
 import dotenv from "dotenv";
 dotenv.config();
 
 export const signup = async (req, res) => {
-    try {
-        const { error } = signupSchema.validate(req.body);
-        if (error) {
-            const message = error.details.map((e) => e.message);
-            return res.status(StatusCodes.BAD_REQUEST).json({ message });
-        }
-        const isExist = await User.findOne({ email: req.body.email });
-        if (isExist) {
-            return res.status(StatusCodes.BAD_GATEWAY).json({
-                message: "Email đã tồn tại !",
-            });
-        }
-        const hashPass = await bcryptjs.hash(req.body.password, 10);
-        const user = await User.create({ ...req.body, password: hashPass });
-        return res.status(StatusCodes.CREATED).json({ user });
-    } catch (error) {
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error });
+  try {
+    const { error } = signupSchema.validate(req.body);
+    if (error) {
+      const message = error.details.map((e) => e.message);
+      return res.status(StatusCodes.BAD_REQUEST).json({ message });
     }
+    const isExist = await User.findOne({ email: req.body.email });
+    if (isExist) {
+      return res.status(StatusCodes.BAD_GATEWAY).json({
+        message: "Email đã tồn tại !",
+      });
+    }
+    const hashPass = await bcryptjs.hash(req.body.password, 10);
+    const user = await User.create({ ...req.body, password: hashPass });
+    return res.status(StatusCodes.CREATED).json({ user });
+  } catch (error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error });
+  }
 };
 
 export const signin = async (req, res) => {
-    try {
-        const { error } = signinSchema.validate(req.body);
-        if (error) {
-            const message = error.details.map((e) => e.message);
-            return res.status(StatusCodes.BAD_REQUEST).json({ message });
-        }
-        const user = await User.findOne({ email: req.body.email });
-        if (!user) {
-            return res.status(StatusCodes.BAD_GATEWAY).json({
-                message: "Email không tồn tại !",
-            });
-        }
-        const isMatch = await bcryptjs.compare(
-            req.body.password,
-            user.password
-        );
-        if (!isMatch) {
-            return res.status(StatusCodes.BAD_GATEWAY).json({
-                message: "Sai mật khẩu !",
-            });
-        }
-        // const token = jwt.sign({ _id: req.body._id }, process.env.SECRET_KEY, {
-        //     expiresIn: "7d",
-        // });
-        return res.status(StatusCodes.ACCEPTED).json({ user });
-    } catch (error) {
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error });
+  try {
+    const { error } = signinSchema.validate(req.body);
+    if (error) {
+      const message = error.details.map((e) => e.message);
+      return res.status(StatusCodes.BAD_REQUEST).json({ message });
     }
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(StatusCodes.BAD_GATEWAY).json({
+        message: "Email không tồn tại !",
+      });
+    }
+    const isMatch = await bcryptjs.compare(req.body.password, user.password);
+    if (!isMatch) {
+      return res.status(StatusCodes.BAD_GATEWAY).json({
+        message: "Sai mật khẩu !",
+      });
+    }
+    // const token = jwt.sign({ _id: req.body._id }, process.env.SECRET_KEY, {
+    //     expiresIn: "7d",
+    // });
+    return res.status(StatusCodes.ACCEPTED).json({ user });
+  } catch (error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error });
+  }
 };
 
 export const update = async (req, res) => {
-    try {
-        const { password, ...updateFields } = req.body;
+  try {
+    const { password, ...updateFields } = req.body;
 
-        if (password) {
-            const hashPass = await bcryptjs.hash(password, 10);
-            updateFields.password = hashPass;
-        }
-
-        const data = await User.findByIdAndUpdate(req.params.id, updateFields, {
-            new: true,
-            runValidators: true,
-        });
-
-        if (!data) {
-            return res
-                .status(StatusCodes.NOT_FOUND)
-                .json({ message: "Người dùng không tồn tại !" });
-        }
-        return res.status(StatusCodes.OK).json({ data });
-    } catch (error) {
-        return res
-            .status(StatusCodes.INTERNAL_SERVER_ERROR)
-            .json({ error: error.message });
+    if (password) {
+      const hashPass = await bcryptjs.hash(password, 10);
+      updateFields.password = hashPass;
     }
+
+    const data = await User.findByIdAndUpdate(req.params.id, updateFields, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!data) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ message: "Người dùng không tồn tại !" });
+    }
+    return res.status(StatusCodes.OK).json({ data });
+  } catch (error) {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: error.message });
+  }
 };
--- controllers/location.js
+++ controllers/location.js
@@ -1,177 +1,177 @@
 import { StatusCodes } from "http-status-codes";
 import Location from "../models/location.js";
 import User from "../models/user.js";
-import { locationSchema } from "../schemas/location.js";
+import { locationSchema } from "../validations/location.js";
 
 export const create = async (req, res) => {
-    const {
-        street,
-        city,
-        state,
-        postalCode,
-        country,
-        recipientName,
-        phoneNumber,
+  const {
+    street,
+    city,
+    state,
+    postalCode,
+    country,
+    recipientName,
+    phoneNumber,
+    userId,
+  } = req.body;
+
+  const { error } = locationSchema.validate(req.body);
+
+  if (error) {
+    const message = error.details.map((e) => e.message);
+    return res.status(StatusCodes.BAD_REQUEST).json({ message });
+  }
+  try {
+    let location = await Location.findOne({ userId });
+
+    if (!location) {
+      location = new Location({
         userId,
-    } = req.body;
-
-    const { error } = locationSchema.validate(req.body);
-
-    if (error) {
-        const message = error.details.map((e) => e.message);
-        return res.status(StatusCodes.BAD_REQUEST).json({ message });
+        locations: [],
+      });
     }
-    try {
-        let location = await Location.findOne({ userId });
 
-        if (!location) {
-            location = new Location({
-                userId,
-                locations: [],
-            });
-        }
+    location.locations.push({
+      street,
+      city,
+      state,
+      postalCode,
+      country,
+      recipientName,
+      phoneNumber,
+    });
 
-        location.locations.push({
-            street,
-            city,
-            state,
-            postalCode,
-            country,
-            recipientName,
-            phoneNumber,
-        });
+    await location.save();
 
-        await location.save();
-
-        return res.status(StatusCodes.CREATED).json({ location });
-    } catch (error) {
-        return res
-            .status(StatusCodes.INTERNAL_SERVER_ERROR)
-            .json({ error: error.message });
-    }
+    return res.status(StatusCodes.CREATED).json({ location });
+  } catch (error) {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: error.message });
+  }
 };
 
 export const get = async (req, res) => {
-    try {
-        const locations = await Location.find(req.params.userId).populate({
-            path: "userId",
-            select: "email name",
-        });
-        if (locations.length === 0) {
-            return res
-                .status(StatusCodes.NOT_FOUND)
-                .json({ message: "Người dùng không có địa chỉ nào !" });
-        }
-
-        return res.status(StatusCodes.OK).json({ locations });
-    } catch (error) {
-        return res
-            .status(StatusCodes.INTERNAL_SERVER_ERROR)
-            .json({ error: error.message });
+  try {
+    const locations = await Location.find(req.params.userId).populate({
+      path: "userId",
+      select: "email name",
+    });
+    if (locations.length === 0) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ message: "Người dùng không có địa chỉ nào !" });
     }
+
+    return res.status(StatusCodes.OK).json({ locations });
+  } catch (error) {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: error.message });
+  }
 };
 
 export const update = async (req, res) => {
-    const {
-        userId,
-        locationId,
-        street,
-        city,
-        state,
-        postalCode,
-        country,
-        recipientName,
-        phoneNumber,
-    } = req.body;
-    try {
-        const { error } = locationSchema.validate(req.body);
-        if (error) {
-            const message = error.details.map((e) => e.message);
-            return res.status(StatusCodes.BAD_REQUEST).json({ message });
-        }
-        let location = await Location.findOne({
-            userId,
-            "locations._id": locationId,
-        });
-
-        if (!location) {
-            return res
-                .status(StatusCodes.NOT_FOUND)
-                .json({ message: "Địa chỉ không tồn tại !" });
-        }
-
-        const address = location.locations.id(locationId);
-
-        if (address) {
-            address.street = street;
-            address.country = country;
-            address.city = city;
-            address.state = state;
-            address.postalCode = postalCode;
-            address.recipientName = recipientName;
-            address.phoneNumber = phoneNumber;
-
-            await location.save();
-
-            return res.status(StatusCodes.OK).json({ location });
-        } else {
-            return res
-                .status(StatusCodes.NOT_FOUND)
-                .json({ message: "Địa chỉ không tồn tại !" });
-        }
-    } catch (error) {
-        return res
-            .status(StatusCodes.INTERNAL_SERVER_ERROR)
-            .json({ error: error.message });
+  const {
+    userId,
+    locationId,
+    street,
+    city,
+    state,
+    postalCode,
+    country,
+    recipientName,
+    phoneNumber,
+  } = req.body;
+  try {
+    const { error } = locationSchema.validate(req.body);
+    if (error) {
+      const message = error.details.map((e) => e.message);
+      return res.status(StatusCodes.BAD_REQUEST).json({ message });
     }
+    let location = await Location.findOne({
+      userId,
+      "locations._id": locationId,
+    });
+
+    if (!location) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ message: "Địa chỉ không tồn tại !" });
+    }
+
+    const address = location.locations.id(locationId);
+
+    if (address) {
+      address.street = street;
+      address.country = country;
+      address.city = city;
+      address.state = state;
+      address.postalCode = postalCode;
+      address.recipientName = recipientName;
+      address.phoneNumber = phoneNumber;
+
+      await location.save();
+
+      return res.status(StatusCodes.OK).json({ location });
+    } else {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ message: "Địa chỉ không tồn tại !" });
+    }
+  } catch (error) {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: error.message });
+  }
 };
 
 export const remove = async (req, res) => {
-    const { userId, locationId } = req.body;
+  const { userId, locationId } = req.body;
 
-    try {
-        let location = await Location.findOne({
-            userId,
-            "locations._id": locationId,
-        });
+  try {
+    let location = await Location.findOne({
+      userId,
+      "locations._id": locationId,
+    });
 
-        if (!location) {
-            return res
-                .status(StatusCodes.NOT_FOUND)
-                .json({ message: "Địa chỉ không tồn tại !" });
-        }
-
-        location.locations = location.locations.filter(
-            (loc) => loc._id.toString() !== locationId
-        );
-
-        await location.save();
-
-        return res
-            .status(StatusCodes.OK)
-            .json({ location, message: "Đã xóa địa chỉ thành công !" });
-    } catch (error) {
-        return res
-            .status(StatusCodes.INTERNAL_SERVER_ERROR)
-            .json({ error: error.message });
+    if (!location) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ message: "Địa chỉ không tồn tại !" });
     }
+
+    location.locations = location.locations.filter(
+      (loc) => loc._id.toString() !== locationId
+    );
+
+    await location.save();
+
+    return res
+      .status(StatusCodes.OK)
+      .json({ location, message: "Đã xóa địa chỉ thành công !" });
+  } catch (error) {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: error.message });
+  }
 };
 
 export const removeLocation = async (req, res) => {
-    try {
-        const location = await Location.findByIdAndDelete(req.params.id);
-        if (!location) {
-            return res
-                .status(StatusCodes.NOT_FOUND)
-                .json({ message: "Địa chỉ không tồn tại !" });
-        }
-
-        return res
-            .status(StatusCodes.OK)
-            .json({ message: "Đã xóa địa chỉ thành công !" });
-    } catch (error) {
-        return res
-            .status(StatusCodes.INTERNAL_SERVER_ERROR)
-            .json({ error: error.message });
+  try {
+    const location = await Location.findByIdAndDelete(req.params.id);
+    if (!location) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ message: "Địa chỉ không tồn tại !" });
     }
+
+    return res
+      .status(StatusCodes.OK)
+      .json({ message: "Đã xóa địa chỉ thành công !" });
+  } catch (error) {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: error.message });
+  }
 };
