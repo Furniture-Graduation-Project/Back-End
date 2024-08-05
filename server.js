5e07fc0189ed6cbc6f719559efd43790d870de18
RiverNguyen <ngiang1309@gmail.com>
2024-08-05 23:20:36 +0700

    remove config folder

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import passport from "passport";
 import "./controllers/passport.js";
 import { app, server } from "./services/socket.js";
 
-import { connectDB } from "./config/connect.js";
+import { connectDB } from "./utils/connect.js";
 import { Route } from "./routers/index.js";
 
 dotenv.config();
