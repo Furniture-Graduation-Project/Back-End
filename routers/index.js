dee7e866a4a6f5389e2568bb4a5a4460244633ba
TruongTaKhac <truongtkph40588@fpt.edu.vn>
2024-10-23 23:36:54 +0700

    Fix Blog

--- routers/index.js
+++ routers/index.js
@@ -14,6 +14,7 @@ import productItemRouter from "./productItem.js";
 import routerPromotion from "./promotion.js";
 import routerVoucher from "./voucher.js";
 import routerWishlist from "./wishlist.js";
+import routerBlog from "./blog.js";
 
 const swaggerDocument = JSON.parse(
   fs.readFileSync(path.resolve("swagger.json"), "utf8")
@@ -29,6 +30,7 @@ export function Route(app) {
   app.use("/wishlist", routerWishlist);
   app.use("/comment", routerComment);
   app.use("/cart", routerCart);
+  app.use("blog", routerBlog);
   app.use("/promotion", routerPromotion);
   app.use("/voucher", routerVoucher);
   app.use("/message", routeMessage);
