409584dead776c1dad56b50d84f867a1e8d06515
PhamHaiHoan <150771122+PhamHaiHoan@users.noreply.github.com>
2024-11-27 22:27:28 +0700

    Sửa controllers/auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -64,17 +64,22 @@ const AuthController = {
 
   update: async (req, res) => {
     try {
+      const {id} = req.params
       const { password, ...updateFields } = req.body;
 
       if (password) {
         const hashPass = await bcryptjs.hash(password, 10);
         updateFields.password = hashPass;
       }
-
+      console.log(req.body);
+      
       const data = await User.findByIdAndUpdate(req.params.id, updateFields, {
         new: true,
         runValidators: true,
       });
+      console.log(req.params.id);
+      console.log(data)
+      
 
       if (!data) {
         return res
