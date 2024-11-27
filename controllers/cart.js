dbd30b6557872f231b4aa17275472a2ad167d3fa
Nguyễn Đình Tuân <junnguyenvizito@gmail.com>
2024-11-27 23:15:49 +0700

    Update cart.js

--- controllers/cart.js
+++ controllers/cart.js
@@ -2,6 +2,7 @@ import CartModel from '../models/cart.js';
 import { StatusCodes } from 'http-status-codes';
 import { createCartSchema, updateCartSchema } from '../validations/cart.js';
 import mongoose from 'mongoose';
+import ProductItemModel from '../models/productItem.js';
 
 const CartController = {
   create: async (req, res) => {
@@ -222,24 +223,32 @@ const CartController = {
     const { productId, productOptionId } = req.params;
     const userId = req.user._id;
     try {
-      const cart = await CartModel.findOneAndUpdate(
-        {
-          UserID: userId,
-          'carts.productId': productId,
-          'carts.productOptionId': productOptionId,
-        },
-        {
-          $inc: { 'carts.$.quantity': 1 },
-        },
-        { new: true },
-      );
-
+      const cart = await CartModel.findOne({
+        UserID: userId,
+        'carts.productId': productId,
+        'carts.productOptionId': productOptionId,
+      });
       if (!cart) {
         return res
           .status(StatusCodes.OK)
           .json({ message: 'Không tìm thấy giỏ hàng hoặc sản phẩm.' });
       }
+      const cartItem = cart.carts.find(
+        (item) =>
+          item.productId.toString() === productId &&
+          item.productOptionId.toString() === productOptionId,
+      );
+      const productItem = await ProductItemModel.findById(productOptionId);
 
+      if (productItem.stock - productItem.outStock < cartItem.quantity + 1) {
+        return res
+          .status(StatusCodes.OK)
+          .json({ message: 'Số lượng trong kho không còn đủ.' });
+      }
+      if (cartItem) {
+        cartItem.quantity += 1;
+      }
+      await cart.save();
       res.status(StatusCodes.OK).json({
         data: cart,
         message: 'Tăng số lượng sản phẩm thành công.',
