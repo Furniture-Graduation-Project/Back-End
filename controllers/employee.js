a2802c38de774398b2612686ebcb7652225adfa3
RiverNguyen <ngiang1309@gmail.com>
2024-12-01 23:01:50 +0700

    FIX: Get blogs

--- controllers/employee.js
+++ controllers/employee.js
@@ -46,7 +46,7 @@ const EmployeeController = {
 
   getLimited: async (req, res) => {
     try {
-      const page = parseInt(req.query.page, 10) || 1;
+      const page = parseInt(req.query.page, 10) + 1 || 1;
       const limit = parseInt(req.query.limit, 10) || 10;
       const skip = (page - 1) * limit;
 
