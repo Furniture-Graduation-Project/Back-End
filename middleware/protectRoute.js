ddb5de7f68f00e8f75243a47301215748b67712a
Your Name <junnguyenvizito@gmail.com>
2024-12-02 19:01:25 +0700

    Repair generateToken , add refresh token for employee

--- middleware/protectRoute.js
+++ middleware/protectRoute.js
@@ -3,11 +3,10 @@ import Employee from "../models/employee.js";
 
 const protectRoute = async (req, res, next) => {
   try {
-    // const authorizationHeader = req.headers["authorization"];
-    // const token = authorizationHeader && authorizationHeader.split(" ")[1];
     const token = req.cookies.accessToken;
     if (!token) {
       return res.status(401).json({ error: "Chưa xác thực - Không có Token" });
+      
     }
 
     let decoded;
