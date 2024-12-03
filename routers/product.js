28d02f658a9e4c1b2f26bfcfd817c497d10cc708
TruongTaKhac <truongtkph40588@fpt.edu.vn>
2024-12-03 23:09:30 +0700

    truongtk updata

--- routers/product.js
+++ routers/product.js
@@ -8,7 +8,8 @@ const router = Router();
 
 router.get("", ProductController.getAll);
 router.get("/new", ProductController.getProductNew);
-router.get("/limited", ProductController.getLimitedAndItems);
+router.get("/limited", ProductController.getLimited);
+router.get("/limited-items", ProductController.getLimitedAndItems);
 router.get("/search", ProductController.getByName);
 router.get("/count-by-category", ProductController.countProductsByCategory);
 router.get("/count-by-material", ProductController.countProductsByMaterial);
