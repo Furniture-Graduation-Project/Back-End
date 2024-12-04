6c7dad6068742a5c7cb71526209738faf9606bfe
TruongTaKhac <truongtkph40588@fpt.edu.vn>
2024-12-04 20:50:37 +0700

    update product

--- controllers/product.js
+++ controllers/product.js
@@ -25,37 +25,13 @@ export const ProductController = {
     }
   },
 
-  getLimited: async (req, res) => {
+  getProductNew: async (req, res) => {
     try {
-      const page = parseInt(req.query.page, 10) + 1 || 1;
-      const limit = parseInt(req.query.limit, 10) || 10;
-      const skip = (page - 1) * limit;
-
-      const categoryId = req.query.categoryId;
-      const materialId = req.query.materialId;
-      const status = req.query.status;
-      const name = req.query.name;
-
-      const query =
-        status == "all" ? {} : status ? { status } : { status: "available" };
-
-      if (categoryId && categoryId != 'all') {
-        query.category = categoryId;
-      }
-
-      if (materialId) {
-        query.material = materialId;
-      }
-
-      if (name) {
-        query.name = { $regex: name, $options: "i" };
-      }
-
-      const products = await ProductModel.find(query)
+      const products = await ProductModel.find({ status: "available" })
+        .sort({ createdAt: -1 })
+        .limit(10)
         .populate("category", "categoryName")
-        .populate("material", "materialName")
-        .skip(skip)
-        .limit(limit);
+        .populate("material", "materialName");
 
       const productsWithPrices = await Promise.all(
         products.map(async (product) => {
@@ -70,25 +46,19 @@ export const ProductController = {
         })
       );
 
-      const totalData = await ProductModel.countDocuments(query);
-
-      const totalPage = limit ? Math.ceil(totalData / limit) : 1;
-
       res.status(StatusCodes.OK).json({
         data: productsWithPrices,
-        totalPage,
-        totalData,
-        message: "Lấy danh sách sản phẩm thành công.",
+        message: "Hiển thị 10 sản phẩm mới nhất thành công.",
       });
     } catch (error) {
       res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
         message: "Có lỗi xảy ra khi lấy thông tin sản phẩm.",
+        message: "Có lỗi xảy ra khi lấy thông tin sản phẩm mới nhất.",
         error: error.message,
       });
     }
   },
-
-  getLimitedAndItems: async (req, res) => {
+  getLimited: async (req, res) => {
     try {
       const page = parseInt(req.query.page, 10) + 1 || 1;
       const limit = parseInt(req.query.limit, 10) || 10;
@@ -153,13 +123,7 @@ export const ProductController = {
           .status(StatusCodes.BAD_REQUEST)
           .json({ message: "Không tìm thấy sản phẩm" });
       }
-<<<<<<< HEAD
-      const product = await ProductModel.findById(id)
-        .populate('category')
-        .populate('material');
-=======
       const product = await ProductModel.findById(id);
->>>>>>> 39351aa (truongtk update)
       if (!product) {
         return res
           .status(StatusCodes.OK)
