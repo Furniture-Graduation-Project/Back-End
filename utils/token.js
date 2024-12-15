f9f50c399c52cf1e38fd17064a5b30f61636bf37
Nguyễn Đình Tuân <junnguyenvizito@gmail.com>
2024-12-15 21:37:51 +0700

    Fix contact mail

--- utils/token.js
+++ utils/token.js
@@ -1,18 +1,18 @@
-import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 export const generateTokenAndSetCookie = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.SECRET_KEY, {
-    expiresIn: "30m",
+    expiresIn: '30m',
   });
 
-  res.cookie("accessToken", token, {
+  res.cookie('accessToken', token, {
     maxAge: 30 * 60 * 1000,
     httpOnly: true,
     sameSite: process.env.SAME_SITE,
-    secure: process.env.NODE_ENV !== "development",
+    secure: process.env.NODE_ENV !== 'development',
   });
 
   return token;
@@ -20,33 +20,33 @@ export const generateTokenAndSetCookie = (userId, res) => {
 
 export const generateRefreshToken = (userId, res) => {
   const refreshToken = jwt.sign({ userId }, process.env.REFRESH_SECRET_KEY, {
-    expiresIn: "7d",
+    expiresIn: '7d',
   });
 
-  res.cookie("refreshToken", refreshToken, {
+  res.cookie('refreshToken', refreshToken, {
     httpOnly: true,
     sameSite: process.env.SAME_SITE,
-    secure: process.env.NODE_ENV !== "development",
+    secure: process.env.NODE_ENV !== 'development',
   });
 
   return refreshToken;
 };
 export const clearCookies = (res) => {
-  res.clearCookie("accessToken", {
+  res.clearCookie('accessToken', {
     httpOnly: true,
     sameSite: process.env.SAME_SITE,
-    secure: process.env.NODE_ENV !== "development",
+    secure: process.env.NODE_ENV !== 'development',
   });
 
-  res.clearCookie("refreshToken", {
+  res.clearCookie('refreshToken', {
     httpOnly: true,
     sameSite: process.env.SAME_SITE,
-    secure: process.env.NODE_ENV !== "development",
+    secure: process.env.NODE_ENV !== 'development',
   });
 };
 
 export const createToken = (user) => {
   return jwt.sign({ email: user.email, id: user._id }, process.env.SECRET_KEY, {
-    expiresIn: "1h",
+    expiresIn: '1h',
   });
 };
