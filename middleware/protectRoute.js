f4efcbbe413a42ba2d4a94421b69f65de55d61c2
Nguyễn Đình Tuân <junnguyenvizito@gmail.com>
2024-12-19 00:04:41 +0700

    fix protect admin

--- middleware/protectRoute.js
+++ middleware/protectRoute.js
@@ -1,36 +1,35 @@
-import jwt from "jsonwebtoken";
-import Employee from "../models/employee.js";
+import jwt from 'jsonwebtoken';
+import Employee from '../models/employee.js';
 
 const protectRoute = async (req, res, next) => {
   try {
     const token = req.cookies.accessToken;
     if (!token) {
-      return res.status(401).json({ error: "Chưa xác thực - Không có Token" });
-      
+      return res.status(401).json({ error: 'Chưa xác thực - Không có Token' });
     }
 
     let decoded;
     try {
       decoded = jwt.verify(token, process.env.SECRET_KEY);
     } catch (error) {
-      if (error.name === "TokenExpiredError") {
+      if (error.name === 'TokenExpiredError') {
         return res
           .status(401)
-          .json({ error: "Token đã hết hạn - Vui lòng đăng nhập lại" });
+          .json({ error: 'Token đã hết hạn - Vui lòng đăng nhập lại' });
       }
       throw error;
     }
 
-    const user = await Employee.findById(decoded.userId).select("-password");
+    const user = await Employee.findById(decoded.userId).select('-password');
     if (!user) {
-      return res.status(404).json({ error: "Người dùng không tìm thấy" });
+      return res.status(401).json({ error: 'Người dùng không tìm thấy' });
     }
 
     req.user = user;
     next();
   } catch (error) {
-    console.error("Lỗi trong middleware protectRoute:", error.message);
-    res.status(500).json({ error: "Lỗi máy chủ nội bộ" });
+    console.error('Lỗi trong middleware protectRoute:', error.message);
+    res.status(500).json({ error: 'Lỗi máy chủ nội bộ' });
   }
 };
 
